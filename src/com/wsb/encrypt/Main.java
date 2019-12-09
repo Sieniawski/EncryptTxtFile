c3080a95460eab7fb618da935fa6fd906ec3489f
Sieniawski <wojciech.sieniawski@csu.local>
2019-12-09 13:09:32 +0100

    adding simple files which encrypt given text, next step is to add file loader

--- src/com/wsb/encrypt/Main.java
+++ src/com/wsb/encrypt/Main.java
@@ -1,8 +1,36 @@
 package com.wsb.encrypt;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.Signature;
+//import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
 
-public class Main {
+    public class Main {
+        public static void main(String args[]) throws Exception{
+            //Creating a Signature object
+            Signature sign = Signature.getInstance("SHA256withRSA");
 
-    public static void main(String[] args) {
-	// write your code here
+            //Creating KeyPair generator object
+            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+
+            //Initializing the key pair generator
+            keyPairGen.initialize(2048);
+
+            //Generating the pair of keys
+            KeyPair pair = keyPairGen.generateKeyPair();
+
+            //Creating a Cipher object
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+            //Initializing a Cipher object
+            cipher.init(Cipher.ENCRYPT_MODE, pair.getPublic());
+
+            //Adding data to the cipher
+            byte[] input = "Welcome".getBytes();
+            cipher.update(input);
+
+            //encrypting the data
+            byte[] cipherText = cipher.doFinal();
+            System.out.println(new String(cipherText, "UTF8"));
+        }
     }
-}
